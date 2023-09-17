017d6de7ae70699a6f9899f78ecb8c001d9792ce
TWINKLE <zhanghjqqqq@163.com>
2023-09-17 17:25:51 +0800

    优化菜单信息

--- page/.vuepress/config_module/headerItem.js
+++ page/.vuepress/config_module/headerItem.js
@@ -1,30 +1,30 @@
 module.exports.headerItem = [
-        [
-            "link",
-            {
-                "rel": "icon",
-                "href": "http://doc.file.htwinkle.cn/2021/07/25/8cc55192402359488043e679d57b07b9.ico"
-            }
-        ],
-        [
-            "link",
-            {
-                "rel": "stylesheet",
-                "href": "//netdna.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
-            }
-        ],
-        [
-            "meta",
-            {
-                "name": "viewport",
-                "content": "width=device-width,initial-scale=1,user-scalable=no"
-            }
-        ],
-        [
-            "meta",
-            {
-                "name": "referer",
-                "content": "no-referrer"
-            }
-        ]
+    [
+        "link",
+        {
+            "rel": "icon",
+            "href": "http://doc.file.htwinkle.cn/2021/07/25/8cc55192402359488043e679d57b07b9.ico"
+        }
+    ],
+    [
+        "link",
+        {
+            "rel": "stylesheet",
+            "href": "//netdna.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
+        }
+    ],
+    [
+        "meta",
+        {
+            "name": "viewport",
+            "content": "width=device-width,initial-scale=1,user-scalable=no"
+        }
+    ],
+    [
+        "meta",
+        {
+            "name": "referer",
+            "content": "no-referrer"
+        }
     ]
+]
--- page/.vuepress/config_module/navItem.js
+++ page/.vuepress/config_module/navItem.js
@@ -1,74 +1,74 @@
 module.exports.navItem = [
+    {
+        "text": "主页",
+        "link": "/",
+        "icon": "fa fa-home"
+    },
+    {
+        "text": "归档",
+        "link": "/timeline/",
+        "icon": "fa fa-map-signs"
+    },
+    {
+        "text": "松花酿酒",
+        "icon": "fa fa-envira",
+        "items": [
             {
-                "text": "主页",
-                "link": "/",
-                "icon": "fa fa-home"
+                "text": "每日一文",
+                "link": "/life/article/"
             },
             {
-                "text": "归档",
-                "link": "/timeline/",
-                "icon": "fa fa-map-signs"
+                "text": "每日一曲",
+                "link": "/life/music/"
             },
             {
-                "text": "松花酿酒",
-                "icon": "fa fa-envira",
-                "items": [
-                    {
-                        "text": "每日一文",
-                        "link": "/life/article/"
-                    },
-                    {
-                        "text": "每日一曲",
-                        "link": "/life/music/"
-                    },
-                    {
-                        "text": "每日一图",
-                        "link": "/life/picture/"
-                    },
-                    {
-                        "text": "每日一视",
-                        "link": "/life/video/"
-                    },
-                    {
-                        "text": "🌸🍀",
-                        "link": "/life/we/"
-                    },
-                    {
-                        "text": "恰若青石的街道向晚",
-                        "link": "/life/tree-hole/"
-                    }
-                ]
+                "text": "每日一图",
+                "link": "/life/picture/"
             },
             {
-                "text": "藏书万卷",
-                "icon": "fa fa-code",
-                "items": [
-                    {
-                        "text": "centos",
-                        "link": "/docs/centos/"
-                    },
-                    {
-                        "text": "学而时习之~",
-                        "link": "/docs/learn/"
-                    },
-                    {
-                        "text": "美工设计",
-                        "link": "/docs/art/"
-                    },
-                    {
-                        "text": "实用拓展",
-                        "link": "/docs/tool/"
-                    }
-                ]
+                "text": "每日一视",
+                "link": "/life/video/"
             },
             {
-                "text": "关于我",
-                "icon": "fa fa-grav",
-                "link": "/about/about/"
+                "text": "恰若青石的街道向晚",
+                "link": "/life/tree-hole/"
             },
             {
-                "text": "github",
-                "icon": "fa fa-github",
-                "link": "https://github.com/h-twinkle"
+                "text": "🌸🍀",
+                "link": "/life/we/"
             }
         ]
+    },
+    {
+        "text": "藏书万卷",
+        "icon": "fa fa-code",
+        "items": [
+            {
+                "text": "centos",
+                "link": "/docs/centos/"
+            },
+            {
+                "text": "学而时习之~",
+                "link": "/docs/learn/"
+            },
+            {
+                "text": "美工设计",
+                "link": "/docs/art/"
+            },
+            {
+                "text": "实用拓展",
+                "link": "/docs/tool/"
+            }
+        ]
+    },
+    {
+        "text": "关于我",
+        "icon": "fa fa-grav",
+        "link": "/about/about/"
+    },
+    {
+        "text": "github",
+        "icon": "fa fa-github",
+        "link": "https://github.com/h-twinkle"
+    }
+]
